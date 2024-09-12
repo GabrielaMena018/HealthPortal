34e4ecaf3f61fe708a8a53640be1a53910f70e6e
Juan421P <20210297@ricaldone.edu.sv>
2024-09-12 15:17:08 -0600

    aaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Controller/Settings/ControllerServerSettings.cs
+++ Controller/Settings/ControllerServerSettings.cs
@@ -21,165 +21,165 @@ namespace HealthPortal.Controller.Settings
         FrmServerSettings frmServerSettings;
         public ControllerServerSettings(FrmServerSettings view)
         {
-            frmServerSettings = view;
+        //    frmServerSettings = view;
 
-            frmServerSettings.txtServerURL.Enter += new EventHandler(EnterTextBox);
-            frmServerSettings.txtDataBase.Enter += new EventHandler(EnterTextBox);
-            frmServerSettings.txtSQLAuth.Enter += new EventHandler(EnterTextBox);
-            frmServerSettings.txtPassword.Enter += new EventHandler(EnterTextBox);
+        //    frmServerSettings.txtServerURL.Enter += new EventHandler(EnterTextBox);
+        //    frmServerSettings.txtDataBase.Enter += new EventHandler(EnterTextBox);
+        //    frmServerSettings.txtSQLAuth.Enter += new EventHandler(EnterTextBox);
+        //    frmServerSettings.txtPassword.Enter += new EventHandler(EnterTextBox);
 
-            frmServerSettings.txtServerURL.Leave += new EventHandler(LeaveTextBox);
-            frmServerSettings.txtDataBase.Leave += new EventHandler(LeaveTextBox);
-            frmServerSettings.txtSQLAuth.Leave += new EventHandler(LeaveTextBox);
-            frmServerSettings.txtPassword.Leave += new EventHandler(LeaveTextBox);
+        //    frmServerSettings.txtServerURL.Leave += new EventHandler(LeaveTextBox);
+        //    frmServerSettings.txtDataBase.Leave += new EventHandler(LeaveTextBox);
+        //    frmServerSettings.txtSQLAuth.Leave += new EventHandler(LeaveTextBox);
+        //    frmServerSettings.txtPassword.Leave += new EventHandler(LeaveTextBox);
 
-            frmServerSettings.rdoTrue.CheckedChanged += new EventHandler(rdTrueMarked);
-            frmServerSettings.rdoFalse.CheckedChanged += new EventHandler(rdFalseMarked);
+        //    frmServerSettings.rdoTrue.CheckedChanged += new EventHandler(rdTrueMarked);
+        //    frmServerSettings.rdoFalse.CheckedChanged += new EventHandler(rdFalseMarked);
 
-            frmServerSettings.btnSaveConfig.Click += new EventHandler(SaveDataBaseConfiguration);
+        //    frmServerSettings.btnSaveConfig.Click += new EventHandler(SaveDataBaseConfiguration);
 
-            frmServerSettings.btnSaveConfig.MouseEnter += new EventHandler(MouseEnterTextButton);
-            frmServerSettings.btnSaveConfig.MouseLeave += new EventHandler(MouseLeaveTextButton);
+        //    frmServerSettings.btnSaveConfig.MouseEnter += new EventHandler(MouseEnterTextButton);
+        //    frmServerSettings.btnSaveConfig.MouseLeave += new EventHandler(MouseLeaveTextButton);
 
-            VerifyOrigin();
-        }
-        private void VerifyOrigin()
-        {
-            if (CurrentUserData.ServerSettingsOrigin == 1)
-            {
-                CommonMethods.EnableFormDrag(frmServerSettings, frmServerSettings);
-                CommonMethods.EnableFormDrag(frmServerSettings, frmServerSettings.picImage);
-                CommonMethods.EnableFormDrag(frmServerSettings, frmServerSettings.pnlTitle);
-            }
-            else
-            {
-                frmServerSettings.txtServerURL.Texts = DTOdbContext.Server;
-                frmServerSettings.txtDataBase.Texts = DTOdbContext.Database;
-                frmServerSettings.txtSQLAuth.Texts = DTOdbContext.User;
-                frmServerSettings.txtPassword.Texts = DTOdbContext.Password;
-                if (string.IsNullOrEmpty(DTOdbContext.User) && string.IsNullOrEmpty(DTOdbContext.Password)) frmServerSettings.rdoFalse.Checked = true;
-            }
-        }
-        private void MouseEnterTextButton(object sender, EventArgs e)
-        {
-            RJButton btn = sender as RJButton;
-            btn.BackColor = Color.FromArgb(31, 43, 91);
-            btn.ForeColor = Color.White;
-        }
-        private void MouseLeaveTextButton(object sender, EventArgs e)
-        {
-            RJButton btn = sender as RJButton;
-            btn.BackColor = Color.White;
-            btn.ForeColor = Color.FromArgb(31, 43, 91);
-        }
-        private void rdFalseMarked(object sender, EventArgs e)
-        {
-            if (frmServerSettings.rdoTrue.Checked == true)
-            {
-                frmServerSettings.pnlAuth.Enabled = true;
-                frmServerSettings.txtSQLAuth.Texts = GetPlaceholderText(frmServerSettings.txtSQLAuth);
-                frmServerSettings.txtPassword.Texts = GetPlaceholderText(frmServerSettings.txtPassword);
-            }
-        }
+        //    VerifyOrigin();
+        //}
+        //private void VerifyOrigin()
+        //{
+        //    if (CurrentUserData.ServerSettingsOrigin == 1)
+        //    {
+        //        CommonMethods.EnableFormDrag(frmServerSettings, frmServerSettings);
+        //        CommonMethods.EnableFormDrag(frmServerSettings, frmServerSettings.picImage);
+        //        CommonMethods.EnableFormDrag(frmServerSettings, frmServerSettings.pnlTitle);
+        //    }
+        //    else
+        //    {
+        //        frmServerSettings.txtServerURL.Texts = DTOdbContext.Server;
+        //        frmServerSettings.txtDataBase.Texts = DTOdbContext.Database;
+        //        frmServerSettings.txtSQLAuth.Texts = DTOdbContext.User;
+        //        frmServerSettings.txtPassword.Texts = DTOdbContext.Password;
+        //        if (string.IsNullOrEmpty(DTOdbContext.User) && string.IsNullOrEmpty(DTOdbContext.Password)) frmServerSettings.rdoFalse.Checked = true;
+        //    }
+        //}
+        //private void MouseEnterTextButton(object sender, EventArgs e)
+        //{
+        //    RJButton btn = sender as RJButton;
+        //    btn.BackColor = Color.FromArgb(31, 43, 91);
+        //    btn.ForeColor = Color.White;
+        //}
+        //private void MouseLeaveTextButton(object sender, EventArgs e)
+        //{
+        //    RJButton btn = sender as RJButton;
+        //    btn.BackColor = Color.White;
+        //    btn.ForeColor = Color.FromArgb(31, 43, 91);
+        //}
+        //private void rdFalseMarked(object sender, EventArgs e)
+        //{
+        //    if (frmServerSettings.rdoTrue.Checked == true)
+        //    {
+        //        frmServerSettings.pnlAuth.Enabled = true;
+        //        frmServerSettings.txtSQLAuth.Texts = GetPlaceholderText(frmServerSettings.txtSQLAuth);
+        //        frmServerSettings.txtPassword.Texts = GetPlaceholderText(frmServerSettings.txtPassword);
+        //    }
+        //}
 
-        private void rdTrueMarked(object sender, EventArgs e)
-        {
-            if (frmServerSettings.rdoFalse.Checked == true)
-            {
-                frmServerSettings.pnlAuth.Enabled = false;
-                frmServerSettings.txtSQLAuth.Clear();
-                frmServerSettings.txtPassword.Clear();
-            }
-        }
-        private void SaveDataBaseConfiguration(object sender, EventArgs e)
-        {
-            try
-            {
-                XmlDocument doc = new XmlDocument();
-                XmlDeclaration decl = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
-                doc.AppendChild(decl);
-                XmlElement root = doc.CreateElement("connection");
-                doc.AppendChild(root);
-                XmlElement server = doc.CreateElement("server");
-                string codedServer = CommonMethods.CodeString(frmServerSettings.txtServerURL.Texts.Trim());
+        //private void rdTrueMarked(object sender, EventArgs e)
+        //{
+        //    if (frmServerSettings.rdoFalse.Checked == true)
+        //    {
+        //        frmServerSettings.pnlAuth.Enabled = false;
+        //        frmServerSettings.txtSQLAuth.Clear();
+        //        frmServerSettings.txtPassword.Clear();
+        //    }
+        //}
+        //private void SaveDataBaseConfiguration(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        XmlDocument doc = new XmlDocument();
+        //        XmlDeclaration decl = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+        //        doc.AppendChild(decl);
+        //        XmlElement root = doc.CreateElement("connection");
+        //        doc.AppendChild(root);
+        //        XmlElement server = doc.CreateElement("server");
+        //        string codedServer = CommonMethods.CodeString(frmServerSettings.txtServerURL.Texts.Trim());
 
-                server.InnerText = codedServer;
-                root.AppendChild(server);
+        //        server.InnerText = codedServer;
+        //        root.AppendChild(server);
 
-                XmlElement Database = doc.CreateElement("database");
-                string codedDatabase = CommonMethods.CodeString(frmServerSettings.txtDataBase.Texts.Trim());
-                Database.InnerText = codedDatabase;
-                root.AppendChild(Database);
-                XmlElement SqlAuth = doc.CreateElement("sqlAuth");
-                XmlElement SqlPassword = doc.CreateElement("sqlPassword");
+        //        XmlElement Database = doc.CreateElement("database");
+        //        string codedDatabase = CommonMethods.CodeString(frmServerSettings.txtDataBase.Texts.Trim());
+        //        Database.InnerText = codedDatabase;
+        //        root.AppendChild(Database);
+        //        XmlElement SqlAuth = doc.CreateElement("sqlAuth");
+        //        XmlElement SqlPassword = doc.CreateElement("sqlPassword");
 
-                if (frmServerSettings.rdoTrue.Checked == true)
-                {
-                    string codedSqlAuth = CommonMethods.CodeString(frmServerSettings.txtSQLAuth.Texts.Trim());
-                    SqlAuth.InnerText = codedSqlAuth;
+        //        if (frmServerSettings.rdoTrue.Checked == true)
+        //        {
+        //            string codedSqlAuth = CommonMethods.CodeString(frmServerSettings.txtSQLAuth.Texts.Trim());
+        //            SqlAuth.InnerText = codedSqlAuth;
 
-                    string codedSqlPassword = CommonMethods.CodeString(frmServerSettings.txtPassword.Texts.Trim());
-                    SqlPassword.InnerText = codedSqlPassword;
-                }
-                else
-                {
-                    SqlAuth.InnerText = string.Empty;
+        //            string codedSqlPassword = CommonMethods.CodeString(frmServerSettings.txtPassword.Texts.Trim());
+        //            SqlPassword.InnerText = codedSqlPassword;
+        //        }
+        //        else
+        //        {
+        //            SqlAuth.InnerText = string.Empty;
 
-                    SqlPassword.InnerText = string.Empty;
-                }
+        //            SqlPassword.InnerText = string.Empty;
+        //        }
 
-                root.AppendChild(SqlAuth);
-                root.AppendChild(SqlPassword);
+        //        root.AppendChild(SqlAuth);
+        //        root.AppendChild(SqlPassword);
 
-                SqlConnection connection = dbContext.testConnection(frmServerSettings.txtServerURL.Texts.Trim(), frmServerSettings.txtDataBase.Texts.Trim(), frmServerSettings.txtSQLAuth.Texts.Trim(), frmServerSettings.txtPassword.Texts.Trim());
+        //        SqlConnection connection = dbContext.testConnection(frmServerSettings.txtServerURL.Texts.Trim(), frmServerSettings.txtDataBase.Texts.Trim(), frmServerSettings.txtSQLAuth.Texts.Trim(), frmServerSettings.txtPassword.Texts.Trim());
 
-                if (connection != null)
-                {
-                    doc.Save("config_server.xml");
-                    DTOdbContext.Server = frmServerSettings.txtServerURL.Texts.Trim();
-                    DTOdbContext.Database = frmServerSettings.txtDataBase.Texts.Trim();
-                    DTOdbContext.User = frmServerSettings.txtSQLAuth.Texts.Trim();
-                    DTOdbContext.Password = frmServerSettings.txtPassword.Texts.Trim();
-                    MessageBox.Show($"El archivo fue creado exitosamente.", "Archivo de configuración", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    frmServerSettings.Dispose();
-                }
-            }
-            catch (XmlException)
-            {
-                MessageBox.Show("No se pudo crear el archivo de configuración.", "Consulte el manual técnico", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-        private void EnterTextBox(object sender, EventArgs e)
-        {
-            CustomTextBox txt = sender as CustomTextBox;
-            if (txt != null)
-            {
-                if (txt.Texts.Trim() == GetPlaceholderText(txt))
-                {
-                    txt.Clear();
-                    txt.ForeColor = Color.FromArgb(31, 43, 91);
-                }
-            }
-        }
-        private void LeaveTextBox(object sender, EventArgs e)
-        {
-            CustomTextBox txt = sender as CustomTextBox;
-            if (txt != null)
-            {
-                if (string.IsNullOrEmpty(txt.Texts))
-                {
-                    txt.Texts = GetPlaceholderText(txt);
-                    txt.ForeColor = Color.FromArgb(142, 202, 230);
-                }
-            }
-        }
-        private string GetPlaceholderText(CustomTextBox txt)
-        {
-            if (txt == frmServerSettings.txtServerURL) return "URL del servidor";
-            if (txt == frmServerSettings.txtDataBase) return "Nombre de la base de datos";
-            if (txt == frmServerSettings.txtSQLAuth) return "Autenticación SQL";
-            if (txt == frmServerSettings.txtPassword) return "Contraseña";
-            return string.Empty;
+        //        if (connection != null)
+        //        {
+        //            doc.Save("config_server.xml");
+        //            DTOdbContext.Server = frmServerSettings.txtServerURL.Texts.Trim();
+        //            DTOdbContext.Database = frmServerSettings.txtDataBase.Texts.Trim();
+        //            DTOdbContext.User = frmServerSettings.txtSQLAuth.Texts.Trim();
+        //            DTOdbContext.Password = frmServerSettings.txtPassword.Texts.Trim();
+        //            MessageBox.Show($"El archivo fue creado exitosamente.", "Archivo de configuración", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //            frmServerSettings.Dispose();
+        //        }
+        //    }
+        //    catch (XmlException)
+        //    {
+        //        MessageBox.Show("No se pudo crear el archivo de configuración.", "Consulte el manual técnico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+        //private void EnterTextBox(object sender, EventArgs e)
+        //{
+        //    CustomTextBox txt = sender as CustomTextBox;
+        //    if (txt != null)
+        //    {
+        //        if (txt.Texts.Trim() == GetPlaceholderText(txt))
+        //        {
+        //            txt.Clear();
+        //            txt.ForeColor = Color.FromArgb(31, 43, 91);
+        //        }
+        //    }
+        //}
+        //private void LeaveTextBox(object sender, EventArgs e)
+        //{
+        //    CustomTextBox txt = sender as CustomTextBox;
+        //    if (txt != null)
+        //    {
+        //        if (string.IsNullOrEmpty(txt.Texts))
+        //        {
+        //            txt.Texts = GetPlaceholderText(txt);
+        //            txt.ForeColor = Color.FromArgb(142, 202, 230);
+        //        }
+        //    }
+        //}
+        //private string GetPlaceholderText(CustomTextBox txt)
+        //{
+        //    if (txt == frmServerSettings.txtServerURL) return "URL del servidor";
+        //    if (txt == frmServerSettings.txtDataBase) return "Nombre de la base de datos";
+        //    if (txt == frmServerSettings.txtSQLAuth) return "Autenticación SQL";
+        //    if (txt == frmServerSettings.txtPassword) return "Contraseña";
+        //    return string.Empty;
         }
     }
 }
--- View/Settings/FrmServerSettings.Designer.cs
+++ View/Settings/FrmServerSettings.Designer.cs
@@ -29,119 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmServerSettings));
-            this.tlpServerConfig = new System.Windows.Forms.TableLayoutPanel();
-            this.pnlTitle = new System.Windows.Forms.Panel();
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.tlpSub = new System.Windows.Forms.TableLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.picImage = new System.Windows.Forms.PictureBox();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.tlpServerConfig.SuspendLayout();
-            this.pnlTitle.SuspendLayout();
-            this.tlpSub.SuspendLayout();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
             this.SuspendLayout();
-            // 
-            // tlpServerConfig
-            // 
-            this.tlpServerConfig.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.tlpServerConfig.ColumnCount = 3;
-            this.tlpServerConfig.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 4.13F));
-            this.tlpServerConfig.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91.74001F));
-            this.tlpServerConfig.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 4.13F));
-            this.tlpServerConfig.Controls.Add(this.pnlTitle, 0, 0);
-            this.tlpServerConfig.Controls.Add(this.tlpSub, 1, 2);
-            this.tlpServerConfig.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tlpServerConfig.Location = new System.Drawing.Point(0, 0);
-            this.tlpServerConfig.Name = "tlpServerConfig";
-            this.tlpServerConfig.RowCount = 4;
-            this.tlpServerConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.02F));
-            this.tlpServerConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.38F));
-            this.tlpServerConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70.22F));
-            this.tlpServerConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.38F));
-            this.tlpServerConfig.Size = new System.Drawing.Size(1090, 705);
-            this.tlpServerConfig.TabIndex = 0;
-            // 
-            // pnlTitle
-            // 
-            this.pnlTitle.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.pnlTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(202)))), ((int)(((byte)(230)))));
-            this.tlpServerConfig.SetColumnSpan(this.pnlTitle, 3);
-            this.pnlTitle.Controls.Add(this.lblTitle);
-            this.pnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlTitle.Location = new System.Drawing.Point(0, 0);
-            this.pnlTitle.Margin = new System.Windows.Forms.Padding(0);
-            this.pnlTitle.Name = "pnlTitle";
-            this.pnlTitle.Size = new System.Drawing.Size(1090, 119);
-            this.pnlTitle.TabIndex = 4;
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(41, 39);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(32, 39, 32, 32);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(457, 41);
-            this.lblTitle.TabIndex = 0;
-            this.lblTitle.Text = "Configuración de Servidor";
-            // 
-            // tlpSub
-            // 
-            this.tlpSub.ColumnCount = 2;
-            this.tlpSub.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tlpSub.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
-            this.tlpSub.Controls.Add(this.panel1, 0, 0);
-            this.tlpSub.Controls.Add(this.tableLayoutPanel1, 1, 0);
-            this.tlpSub.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tlpSub.Location = new System.Drawing.Point(45, 163);
-            this.tlpSub.Margin = new System.Windows.Forms.Padding(0);
-            this.tlpSub.Name = "tlpSub";
-            this.tlpSub.RowCount = 1;
-            this.tlpSub.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tlpSub.Size = new System.Drawing.Size(999, 495);
-            this.tlpSub.TabIndex = 5;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.picImage);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(299, 495);
-            this.panel1.TabIndex = 0;
-            // 
-            // picImage
-            // 
-            this.picImage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.picImage.Image = global::HealthPortal.Properties.Resources.serverSettings;
-            this.picImage.Location = new System.Drawing.Point(0, 0);
-            this.picImage.Name = "picImage";
-            this.picImage.Size = new System.Drawing.Size(299, 495);
-            this.picImage.TabIndex = 0;
-            this.picImage.TabStop = false;
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.499108F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.00179F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.499108F));
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(299, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 5;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.1236F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.25843F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.24719F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.24719F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.1236F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(700, 495);
-            this.tableLayoutPanel1.TabIndex = 1;
             // 
             // FrmServerSettings
             // 
@@ -151,31 +39,16 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(202)))), ((int)(((byte)(230)))));
             this.ClientSize = new System.Drawing.Size(1090, 705);
-            this.Controls.Add(this.tlpServerConfig);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimumSize = new System.Drawing.Size(986, 705);
             this.Name = "FrmServerSettings";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Configuración de Servidor";
-            this.tlpServerConfig.ResumeLayout(false);
-            this.pnlTitle.ResumeLayout(false);
-            this.pnlTitle.PerformLayout();
-            this.tlpSub.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.Text = "vv";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tlpServerConfig;
-        public System.Windows.Forms.Panel pnlTitle;
-        private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.TableLayoutPanel tlpSub;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox picImage;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
