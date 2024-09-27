c3299d6151e989604fa3e1cbede7616c2fb33eda
Juan421P <98666199+Juan421P@users.noreply.github.com>
2024-09-27 14:41:51 -0600

    delete por medio del admin

---
+++ View/PatientAdministration/FrmDeleetPatientAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthPortal.View.PatientAdministration
+{
+    public partial class FrmDeleetPatientAdmin : Form
+    {
+        public FrmDeleetPatientAdmin()
+        {
+            InitializeComponent();
+        }
+    }
+}
